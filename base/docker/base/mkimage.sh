3c398aa23af1975e2fdb410154b82bc050d64043
mitsuo yamamoto <mitsuo.yamamoto@gmail.com>
2017-02-17 16:29:17 +0900

    change dockerfile

--- base/docker/base/mkimage.sh
+++ base/docker/base/mkimage.sh
@@ -1,1 +1,1 @@
-docker build -t miyamamoto/base:1.0 .
+docker build -t miyamamoto/base:latest .
--- base/docker/notebook/run_notebook.sh
+++ base/docker/notebook/run_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /bin/bash -c "source activate py35 && jupyter notebook"
+exec /bin/bash -c "source activate py35 && THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32 jupyter notebook"
